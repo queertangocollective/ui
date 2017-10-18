608f8a07a4d92785b8d43f6c9c7c07ea097f344c
Tim Evans <tim.c.evans@me.com>
2017-10-18 00:14:20 -0400

    add {{dialog-box}}

---
+++ app/components/dialog-box.js
@@ -0,0 +1,1 @@
+export { default } from 'ui/components/dialog-box/component';
--- app/transitions.js
+++ app/transitions.js
@@ -1,9 +1,26 @@
+import config from './config/environment';
+
 export default function(){
-  // Add your transitions here, like:
-  //   this.transition(
-  //     this.fromRoute('people.index'),
-  //     this.toRoute('people.detail'),
-  //     this.use('toLeft'),
-  //     this.reverse('toRight')
-  //   );
+  let duration = config.environment === 'test' ? 0 : 150;
+  let slow = config.environment === 'test' ? 0 : 600;
+  let easing = 'ease-in-out';
+
+  this.transition(
+    this.hasClass('liquid-pop-over'),
+    this.toValue(true),
+    this.use('toolTip', { duration, easing }),
+    this.reverse('toolTip', { duration, easing })
+  );
+
+  this.transition(
+    this.childOf('.search-field'),
+    this.toValue(true),
+    this.use('fade', { duration }),
+    this.reverse('fade', { duration })
+  );
+
+  this.transition(
+    this.hasClass('snack-bar'),
+    this.use('toaster', { duration: slow, easing: [600, 30] })
+  );
 }
