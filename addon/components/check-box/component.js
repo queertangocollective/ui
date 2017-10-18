493ea27884b3c94762ef8fd25b2aa1a371b70327
Tim Evans <tim.c.evans@me.com>
2017-10-17 23:29:25 -0400

    add {{check-box}}

--- addon/components/check-box/component.js
+++ addon/components/check-box/component.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
 import { get, computed } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
+import layout from './template';
 
 export default Component.extend({
+  layout,
   tagName: 'span',
   classNames: ['check-box'],
   inputId: computed({
---
+++ app/components/check-box.js
@@ -0,0 +1,1 @@
+export { default } from 'ui/components/check-box/component';
