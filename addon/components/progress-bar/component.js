4eaa6c016b31320dc69265d4b1e21ac1e8aa17e6
Tim Evans <tim.c.evans@me.com>
2017-10-22 22:43:07 -0400

    add {{progress-bar}}

---
+++ addon/components/progress-bar/component.js
@@ -0,0 +1,25 @@
+import Component from '@ember/component';
+import { set, get, computed } from '@ember/object';
+import { htmlSafe } from '@ember/string';
+import { later } from '@ember/runloop';
+
+export default Component.extend({
+  tagName: '',
+
+  didReceiveAttrs() {
+    if (get(this, 'progress') > 0 &&
+        get(this, 'value') === 0) {
+      later(() => {
+        if (this.isDestroyed) { return; }
+        set(this, 'progress', 0);
+      }, 1000);
+    } else {
+      set(this, 'progress', get(this, 'value'));
+    }
+  },
+
+  style: computed('progress', function () {
+    return htmlSafe(`width: ${get(this, 'progress')}%`);
+  })
+});
+
