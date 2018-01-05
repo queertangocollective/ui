6a80d939a949098390ec97b31960ca0e32fcfd63
Tim Evans <tim.c.evans@me.com>
2018-01-04 20:44:46 -0500

    update addon configuration

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 /* eslint-env node */
 'use strict';
+const Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: '@queertangocollective/ui',
+
+  addonPath(path) {
+    return `node_modules/@queertangocollective/ui/${path}`;
+  },
+
   included() {
     this._super.included.apply(this, arguments);
+  },
+
+  treeForPublic() {
+    this._requireBuildPackages();
+
+    if (!tree) {
+      return tree;
+    }
+
+    return new Funnel(tree, {
+      srcDir: '/',
+      destDir: '/'
+    });
   }
 };
