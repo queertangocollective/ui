9801403819084b81f38f1a2ba74a3ced08dbd089
Tim Evans <tim.c.evans@me.com>
2018-01-06 12:19:45 -0500

    fix submit-button

--- addon/components/submit-button/component.js
+++ addon/components/submit-button/component.js
@@ -2,19 +2,24 @@ import Component from '@ember/component';
 import { set, get } from '@ember/object';
 import RSVP from 'rsvp';
 import { task, timeout } from 'ember-concurrency';
+import layout from './template';
 
 export default Component.extend({
   tagName: 'button',
 
+  attributeBindings: ['type'],
+
+  type: 'submit',
+
   submit: task(function *(submit) {
-    this.$().css('width', this.$().outerWidth());
+    get(this, 'element').styles.width = get(this, 'element').getBoundingClientRect().width + 'px';
     set(this, 'isProcessing', true);
     try {
       yield RSVP.all([submit(), timeout(500)]);
     } finally {
       set(this, 'isProcessing', false);
       yield timeout(500);
-      this.$().removeAttr('style');
+      get(this, 'element').styles.width = null;
     }
   }),
 
---
+++ app/components/submit-button.js
@@ -0,0 +1,1 @@
+export { default } from '@queertangocollective/ui/components/submit-button/component';
