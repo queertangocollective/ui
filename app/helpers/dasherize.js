0f42818fbe587893d87e72c38e271cd9a442bbdc
Tim Evans <tim.c.evans@me.com>
2018-01-06 20:23:09 -0500

    expose {{dasherize}} via addon

--- app/helpers/dasherize.js
+++ app/helpers/dasherize.js
@@ -1,6 +1,1 @@
-import { helper } from '@ember/component/helper';
-import { dasherize } from '@ember/string';
-
-export default helper(function ([string]) {
-  return dasherize((string || '').replace(/\./g, '-').replace(/\//g, '-'));
-});
+export { default } from '@queertangocollective/ui/helpers/dasherize';
