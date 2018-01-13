8b058200b59a5b792b8198ec31a36496a836b84d
Tim Evans <tim.c.evans@me.com>
2018-01-13 00:04:16 -0500

    add flow for tooltip

--- app/flows.js
+++ app/flows.js
@@ -2,3 +2,8 @@ export function dropdown() {
   return this.orientBelow.andSnapTo(this.center, this.rightEdge, this.leftEdge)
    .then(this.orientAbove.andSnapTo(this.center, this.rightEdge, this.leftEdge));
 }
+
+export function popup() {
+  return this.orientAbove.andSnapTo(this.center, this.rightEdge, this.leftEdge)
+   .then(this.orientBelow.andSnapTo(this.center, this.rightEdge, this.leftEdge));
+}
