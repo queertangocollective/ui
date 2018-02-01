bd87322369ea9cabd1b592261a96779da7a45ca0
Tim Evans <tim.c.evans@me.com>
2018-01-31 21:45:54 -0500

    use an expanding menu instead of a pop over

--- addon/components/auto-complete/component.js
+++ addon/components/auto-complete/component.js
@@ -7,33 +7,25 @@ export default Component.extend({
   layout,
   classNames: ['auto-complete'],
 
-  searchId: computed({
-    get() {
-      return 'search-' + guidFor(this);
-    }
+  searchId: computed(function () {
+    return 'search-' + guidFor(this);
   }),
 
   actions: {
     query(query) {
       if (query === '') {
-        get(this, 'popover').hide();
         get(this, 'onchange')(null);
+        set(this, 'options', [])
       } else {
         get(this, 'onquery')({ text: query }).then((options) => {
           set(this, 'options', options);
-          if (get(options, 'length')) {
-            get(this, 'popover').show();
-            get(this, 'popover').retile();
-          } else {
-            get(this, 'popover').hide();
-          }
         });
       }
     },
 
     change(value) {
-      get(this, 'popover').hide();
       get(this, 'onchange')(value);
+      set(this, 'options', []);
     }
   }
 
