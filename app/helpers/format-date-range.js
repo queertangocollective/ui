a5acdda089cd59e327d1043a3788004e01fafda4
Tim Evans <tim.c.evans@me.com>
2018-02-16 21:30:44 -0500

    fix date range formats for time ranges

--- app/helpers/format-date-range.js
+++ app/helpers/format-date-range.js
@@ -2,12 +2,12 @@ import { helper } from '@ember/component/helper';
 import moment from 'moment';
 
 export function formatDateRange(startDate, endDate) {
-  if (moment(startDate).isSame(moment(endDate))) {
-    return moment(startDate).format('MMMM D, YYYY');
-  } else if (moment(startDate).diff(moment(endDate), 'months') === 0) {
-    return `${moment(startDate).format("MMMM D")} - ${moment(endDate).format('D, YYYY')}`;
+  if (moment(startDate).isSame(moment(endDate), 'day')) {
+    return moment(startDate).format('MMMM D, YYYY h:MMa') + ' &endash; ' + moment(endDate).format('h:MMa');
+  } else if (moment(startDate).isSame(moment(endDate), 'month')) {
+    return `${moment(startDate).format("MMMM D h:MMa")} &endash; ${moment(endDate).format('D, YYYY h:MMa')}`;
   } else {
-    return `${moment(startDate).format("MMMM D, YYYY")} - ${moment(endDate).format('MMMM D, YYYY')}`;
+    return `${moment(startDate).format("MMMM D, YYYY h:MMa")} &endash; ${moment(endDate).format('MMMM D, YYYY h:MMa')}`;
   }
 }
 
