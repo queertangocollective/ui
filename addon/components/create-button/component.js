f6c7d8c914e2700d6352c55dc94eb4b9f46bfdec
Tim Evans <tim.c.evans@me.com>
2018-05-22 22:02:35 -0400

    fix styles for create-button

--- addon/components/create-button/component.js
+++ addon/components/create-button/component.js
@@ -1,10 +1,23 @@
 import Component from '@ember/component';
 import { set } from '@ember/object';
+import { run } from '@ember/runloop';
 import layout from './template';
+import { fadeIn, fadeOut } from 'ember-animated/motions/opacity';
 
 export default Component.extend({
   layout,
-  tagName: '',
+  classNames: ['create-button'],
+  type: 'text',
+
+  *fade({ insertedSprites, removedSprites }) {
+    insertedSprites.map(fadeIn);
+    removedSprites.map(fadeOut);
+  },
+
+  destroy() {
+    document.body.removeEventListener('click', this.documentClick);
+    this._super();
+  },
 
   actions: {
     submit(value) {
@@ -12,9 +25,20 @@ export default Component.extend({
         .catch(() => {})
         .finally(() => set(this, 'isCreating', false));
     },
+
     create(evt) {
       evt.preventDefault();
       set(this, 'isCreating', true);
+
+      this.documentClick = (evt) => {
+        if (this.element !== evt.target &&
+            !this.element.contains(evt.target)) {
+          run(() => {
+            set(this, 'isCreating', false);
+          });
+        }
+      };
+      document.body.addEventListener('mousedown', this.documentClick);
     }
   }
 }).reopenClass({
