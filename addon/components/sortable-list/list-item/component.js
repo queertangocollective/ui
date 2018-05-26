6cbf19f371d19254769e675eefb42c00d92aee52
Tim Evans <tim.c.evans@me.com>
2018-05-25 23:28:58 -0400

    add mobile views for tabular / sortable lists

--- addon/components/sortable-list/list-item/component.js
+++ addon/components/sortable-list/list-item/component.js
@@ -3,12 +3,28 @@ import { computed } from '@ember/object';
 import layout from './template';
 
 export default Component.extend({
-  tagName: '',
   layout,
   sortable: true,
   sortDesc: computed('sortAsc', function () {
     return `-${this.sortAsc}`;
-  })
+  }),
+  didRender() {
+    if (!this.item && this.element) {
+      if (this.sort === this.sortAsc) {
+        this.list.set('currentSort', {
+          html: this.element.querySelector('span').innerHTML,
+          direction: 'asc',
+          reverse: this.sortDesc
+        });
+      } else if (this.sort === this.sortDesc) {
+        this.list.set('currentSort', {
+          html: this.element.querySelector('span').innerHTML,
+          direction: 'desc',
+          reverse: this.sortAsc
+        });
+      }
+    }
+  }
 }).reopenClass({
   positionalParams: ['sortAsc', 'sortDesc']
 });
