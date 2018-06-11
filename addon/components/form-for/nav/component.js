461651e5dc12e43679516ad450722a3047eb5b48
Tim Evans <tim.c.evans@me.com>
2018-06-11 00:09:17 -0400

    fix nested forms with form-for

--- addon/components/form-for/nav/component.js
+++ addon/components/form-for/nav/component.js
@@ -1,9 +1,14 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from './template';
 
 export default Component.extend({
   layout,
   tagName: '',
+
+  hasChanges: computed('changeset.hasChanges', 'nestedForms.@each.changeset.hasChanges', function () {
+    return this.changeset.hasChanges || this.nestedForms.someBy('changeset.hasChanges');
+  }),
 
   stop(evt) {
     evt.preventDefault();
